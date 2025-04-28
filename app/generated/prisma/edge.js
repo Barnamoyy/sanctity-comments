ac757f3dc7a7dd10e54166dd2064fdaf64b5d5e7
HalkatHaathi <75882515+Barnamoyy@users.noreply.github.com>
2025-04-28 22:37:25 +0530

    prisma

--- app/generated/prisma/edge.js
+++ app/generated/prisma/edge.js
@@ -185,11 +185,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://barnamoy:iueHMqTOL4OPCXWL7fbi@instance.cpk0ygkimmyu.ap-south-1.rds.amazonaws.com/commentsDB"
+        "value": null
       }
     }
   },
